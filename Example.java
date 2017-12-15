7be17e7ece1a15be3550a7c88b8e48be14126a79
BlazingForrest <34559405+BlazingForrest@users.noreply.github.com>
2017-12-15 15:05:19 +1300

    Example of the ArteaAPI library

--- Example.java
+++ Example.java
@@ -2,12 +2,15 @@ import arteaAPI.IFTTT;
 
 public class Example {
 	
-	// You can find your IFTTT maker key by searching 'webhooks' on IFTTT, clicking 'settings' on the top-right and
-	// copying the part of the url after https://maker.ifttt.com/use/
-	// For example, your URL might be 1234567890 because the URL is https://maker.ifttt.com/use/1234567890
-	
 	public static void main(String[] args) {
-		String[] values = {"1", "2"}; // Data you want to send to IFTTT server, please note that IFTTT can only recieve 3 values
+		// Data you want to send to IFTTT server, please note that IFTTT can only recieve 3 values
+		
+		String[] values = {"1", "2"};
+		
+		// You can find your IFTTT maker key by searching 'webhooks' on IFTTT, clicking 'settings' on the top-right and
+		// copying the part of the url after https://maker.ifttt.com/use/
+		// For example, your URL might be 1234567890 because the URL is https://maker.ifttt.com/use/1234567890
+		
 		IFTTT.sendWebHook("event_name", "IFTTT_Maker_key", values);
 	}
 
