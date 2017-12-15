be9875f11691ed1ec6535e5f0d660e0f8261c840
BlazingForrest <34559405+BlazingForrest@users.noreply.github.com>
2017-12-15 14:57:31 +1300

    Update Example.java

--- Example.java
+++ Example.java
@@ -5,6 +5,7 @@ public class Example {
 	// You can find your IFTTT maker key by searching 'webhooks' on IFTTT, clicking 'settings' on the top-right and
 	// copying the part of the url after https://maker.ifttt.com/use/
 	// For example, your URL might be 1234567890 because the URL is https://maker.ifttt.com/use/1234567890
+	
 	public static void main(String[] args) {
 		String[] values = {"1", "2"}; // Data you want to send to IFTTT server
 		IFTTT.sendWebHook("event_name", "IFTTT_Maker_key", values);
